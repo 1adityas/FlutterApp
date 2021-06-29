0d4328f577dbe55b5816a180b54aa47f3caf0a8b
aditya <1adityapsingh@gmail.com>
2021-06-29 20:43:55 +0530

    font-style

--- lib/notes_4yr.dart
+++ lib/notes_4yr.dart
@@ -5,6 +5,7 @@ import 'package:responsive_screen/responsive_screen.dart';
 import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'dart:math';
+import 'package:google_fonts/google_fonts.dart';
 
 class detail {
   String subject, asset, url;
@@ -45,10 +46,15 @@ class _yearfourState extends State<yearfour>
           backgroundColor: Colors.grey[200],
           appBar: AppBar(
             centerTitle: true,
-
             backgroundColor: Colors.orange,
             title: Text(
               'Fourth Year',
+              style: GoogleFonts.castoro(
+                textStyle: Theme.of(context).textTheme.headline4,
+                fontSize: 48,
+                fontWeight: FontWeight.w700,
+                fontStyle: FontStyle.italic,
+              ),
             ),
             bottom: TabBar(
               indicatorColor: Colors.white,
